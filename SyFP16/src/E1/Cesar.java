aaf2ff5ca1fdc11c38a9b34a4abad966f5075ede
LuisCalderon <LuisCalderon@10.20.11.62>
2016-05-30 11:59:52 -0500

    Intento1

--- SyFP16/src/E1/Cesar.java
+++ SyFP16/src/E1/Cesar.java
@@ -1,6 +1,21 @@
 package E1;
-
-
 public class Cesar {
+    public static void main(String[] args) {
+        
+        String mensaje = "Hola amorcito";
+        
+        char array [] = mensaje.toCharArray();
+        
+        for(int i=0;i<array.length; i++ )
+        {
+            array[i]= (char)(array[i]+(char)1);
+        }
+        
+        
+        String cifrado = String.valueOf(array);
+        
+        
+        System.out.println(cifrado);
+    }
    
 }
