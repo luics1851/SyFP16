a639b920ec4b0eaaa4dfed7ac345fd6b548cc32d
LuisCalderon <LuisCalderon@10.20.11.62>
2016-06-16 11:13:41 -0500

    EMT

---
+++ SyFP16/src/EMT/Ccircula.java
@@ -0,0 +1,87 @@
+
+package EMT;
+
+
+public class Ccircula implements Icircula {
+
+    @Override
+    public String getHoyNoCircula() {
+        return "hoy no circula engomado verde y terminacion 1 y 2";
+    }
+
+    @Override
+    public String getByPlaca(String a) {
+        if(a=="5"||a=="6")
+        {
+            return "Lunes";
+        }
+        if(a=="9"||a=="0")
+        {
+            return "Viernes";
+        }
+        if(a=="7"||a=="8")
+        {
+            return "Martes";
+        }
+        if(a=="3"||a=="4")
+        {
+            return "Miercoles";
+        }
+        if(a=="1"||a=="2")
+        {
+            return "Jueves";
+        }
+        return"";
+    }
+
+    @Override
+    public String getTerminacionPlaca(String b) {
+        if(b=="Lunes")
+        {
+            return "5y6";
+        }
+        if(b=="Viernes")
+        {
+            return "9y0";
+        }
+        if(b=="Martes")
+        {
+            return "7y8";
+        }
+        if(b=="Miercoles")
+        {
+            return "3y4";
+        }
+        if(b=="Jueves")
+        {
+            return "1y2";
+        }
+        return"";
+    }
+
+    @Override
+    public String getColor(String c) {
+        if(c=="Amarillo")
+        {
+            return "Lunes";
+        }
+        if(c=="Azul")
+        {
+            return "Viernes";
+        }
+        if(c=="Rosa")
+        {
+            return "Martes";
+        }
+        if(c=="Rojo")
+        {
+            return "Miercoles";
+        }
+        if(c=="Verde")
+        {
+            return "Jueves";
+        }
+        return"";
+    }
+    
+}
---
+++ SyFP16/src/EMT/Icircula.java
@@ -0,0 +1,9 @@
+
+package EMT;
+
+public interface Icircula {
+    public String getHoyNoCircula();
+    public String getByPlaca(String a);
+    public String getTerminacionPlaca(String b);
+    public String getColor(String c);
+    }
